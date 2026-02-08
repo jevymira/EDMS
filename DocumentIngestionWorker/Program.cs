2a7690450c778a880945222cd3d3a45715435037
Jevy Miranda <jevymira@outlook.com>
2026-02-08 15:20:31 -0800

    chore(worker): remove config for unused features

--- DocumentIngestionWorker/Program.cs
+++ DocumentIngestionWorker/Program.cs
@@ -22,18 +22,8 @@ namespace DocumentIngestionWorker
                     services.AddMassTransit(x =>
                     {
                         x.SetKebabCaseEndpointNameFormatter();
-
-                        // By default, sagas are in-memory, but should be changed to a durable
-                        // saga repository.
-                        x.SetInMemorySagaRepositoryProvider();
-
                         var entryAssembly = Assembly.GetEntryAssembly();
-
                         x.AddConsumers(entryAssembly);
-                        x.AddSagaStateMachines(entryAssembly);
-                        x.AddSagas(entryAssembly);
-                        x.AddActivities(entryAssembly);
-
                         x.UsingRabbitMq((context, cfg) =>
                         {
                             cfg.ConfigureEndpoints(context);
