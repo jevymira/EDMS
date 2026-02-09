d8ceb893640660737422dcd1739dfe2ec13a37aa
Jevy Miranda <jevymira@outlook.com>
2026-02-09 12:23:02 -0800

    feat: define Document model and EF Core DbContext

---
+++ Infrastructure/DocumentDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class DocumentDbContext : DbContext
+{
+    public DbSet<Document> Documents { get; set; }
+}
