0f90b50c4e7e68eed1d5bb1eb3dd400e054f441d
Jevy Miranda <jevymira@outlook.com>
2026-02-09 17:18:39 -0800

    fix: expose public DbContext constructor
    This passes configuration of the context from AddDbContext.

--- Infrastructure/DocumentDbContext.cs
+++ Infrastructure/DocumentDbContext.cs
@@ -5,5 +5,7 @@ namespace Infrastructure;
 
 public class DocumentDbContext : DbContext
 {
+    public DocumentDbContext(DbContextOptions<DocumentDbContext> options) : base(options) { }
+
     public DbSet<Document> Documents { get; set; }
 }
